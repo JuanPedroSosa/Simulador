0406015177f57b4040006a28950829dc8db3a3a1
Juan Pedro Sosa <juanpedrososa@outlook.com>
2021-09-15 21:32:35 -0300

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
-# Node-Handlebars
-1. Clone repo
-2. Run npm i
-3. Run node index.js
+1. Run npm i
+2. Run node index.js
--- index.js
+++ index.js
@@ -1,17 +1,59 @@
-const app = require("express")();
+const express = require('express');
+const app = express();
+const router = express.Router();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+// const app = require("express")();
 var path = require("path");
-
+const port = process.env.PORT || 5000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// settings
+app.set('port', port);
+       // set our port
 //view engine setup
 app.set("views", path.join(__dirname, "views")); //setting views directory for views.
 app.set("view engine", "hbs"); //setting view engine as handlebars
 
 app.get("/", (req, res) => {
-	let peopleList = getRandomList();
-	res.render("index", { people: peopleList }); //passing list of people to our index.hbs file.
+	arr = [new Date(), "Disponible"];
+	//res.render("index", { msg: arr }); //passing list of people to our index.hbs file.
+	res.sendFile(path.join(__dirname + '/views/index.html'));
 });
+
+app.get("/status", (req, res) => {
+	arr = [new Date(), "Estado OK"];
+	res.render("index", { msg: arr }); //passing list of people to our index.hbs file.
+});
+
+
+app.post("/", (req, res) => {
+	console.log("body:", req.body.msg);
+	arr = [new Date(), req.body.msg];
+	let msg = `${new Date()}: ${req.body.msg}`;
+	console.log("arr:", arr);
+	io.emit("inflar", msg);
+	//res.sendFile(path.join(__dirname + '/views/edit1.html'));
+	res.status(200).json({ msg: msg }); //passing list of people to our index.hbs file.
+
+});
+
 let getRandomList = () => {
 	let list = ["ada", "turing", "lovelace", "neumann", "gracehopper"];
 	let limit = Math.floor(Math.random() * (list.length - 1 - 0) + 0); //generating random number between 0 & 4
 	return list.slice(limit);
 };
-app.listen(5000);
+
+app.use("/", router);
+
+io.on('connection', (socket) => {
+		console.log('a user connected');
+});
+// starting the server
+server.listen(app.get('port'), () => {
+	// Conectase a la base de datos
+  console.log(`Server on port ${app.get('port')}`);
+});
+
